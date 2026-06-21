f29f209f54a0b8e622ac6642d23a7c334060c19f
Luciano <andreloluciano91@gmail.com>
2026-06-21 20:30:09 -0300

    funcion ordenarPor

--- frontend/tpi-prog3/src/pages/store/home/home.ts
+++ frontend/tpi-prog3/src/pages/store/home/home.ts
@@ -7,6 +7,7 @@ const inputBuscar = document.getElementById("buscar") as HTMLInputElement;
 const contenedor = document.getElementById("contenedor-productos") as HTMLDivElement; // contenedor
 const listaCategorias = document.getElementById("lista-categorias") as HTMLUListElement; //importo categorias
 const contadorProductos = document.getElementById("contador-productos") as HTMLParagraphElement; // contador de productos
+const selectOrdenar = document.getElementById("ordenar") as HTMLSelectElement;
 checkAuhtUser(
   "/src/pages/auth/login/login.html",
   "/src/pages/admin/home/home.html",
@@ -16,6 +17,7 @@ checkAuhtUser(
 
 let productos: Product[] = [];
 let categorias: Category[] = [];
+let productosMostrados: Product[] = [];
 
 // funcion para agregar productos al carrito
 const agregarAlCarrito = (producto: Product) => { 
@@ -38,6 +40,7 @@ const agregarAlCarrito = (producto: Product) => {
 
 // funcion render, recorre los productos y los mete dentr odel contenedor
 const renderProductos = (productos: Product[]) => { 
+  productosMostrados = productos; // guardo los productos que se estan mostrando
   contenedor.innerHTML = ""; // limpia antes de renderizar
   contadorProductos.textContent = `Productos encontrados: ${productos.length}`; // contador
 
@@ -130,6 +133,37 @@ inputBuscar.addEventListener("input", () => { // listener para cuando el usuario
 
   // renderizo resultados
   renderProductos(productosFiltrados);
+});
+
+// funcion para ordenar productos
+const ordenarProductos = (productos: Product[]) => {
+  const orden = selectOrdenar.value;
+
+  const productosOrdenados = [...productos]; // copio array para no modificar el original
+
+  if (orden === "nombre-asc") {
+    productosOrdenados.sort((a, b) => a.nombre.localeCompare(b.nombre));
+  }
+
+  if (orden === "nombre-desc") {
+    productosOrdenados.sort((a, b) => b.nombre.localeCompare(a.nombre));
+  }
+
+  if (orden === "precio-asc") {
+    productosOrdenados.sort((a, b) => a.precio - b.precio);
+  }
+
+  if (orden === "precio-desc") {
+    productosOrdenados.sort((a, b) => b.precio - a.precio);
+  }
+
+  return productosOrdenados;
+};
+
+// evento para ordenar productos
+selectOrdenar.addEventListener("change", () => {
+  const productosOrdenados = ordenarProductos(productosMostrados);
+  renderProductos(productosOrdenados);
 });
 
 // cargo productos y categorias desde los json
